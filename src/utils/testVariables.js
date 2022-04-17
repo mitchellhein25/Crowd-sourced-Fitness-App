f983ed6844b2ace8d2a5a929cf120d3c3d1c9668
mitchellhein25 <mitchelldalehein25@gmail.com>
2022-04-17 11:44:15 -0500

    Created initial draft of New Challenge Form

--- src/utils/testVariables.js
+++ src/utils/testVariables.js
@@ -1,6 +1,4 @@
-export const testAccountEmail = 'jesttest1@email.com';
-export const testAccountPass = 'jesttest1';
 // export const testAccountEmail = 'test@email.com';
 // export const testAccountPass = 'testpass';
-// export const testAccountEmail = 'test2@email.com';
-// export const testAccountPass = 'testpass2';
+export const testAccountEmail = 'test2@email.com';
+export const testAccountPass = 'testpass2';
