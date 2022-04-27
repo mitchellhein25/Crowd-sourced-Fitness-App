477ef93a40ac9db56a779d3c76c46477f15fadc8
Uriel Rodriguez <urielrodriguez@Uriels-Air.lan>
2022-04-27 15:51:45 -0700

    Implemented UI and backend for the active challenges screen. Need to fix the hard-coded user key

--- src/screens/activeChallenges.js
+++ src/screens/activeChallenges.js
@@ -1,20 +1,202 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { white } from '../utils/globalStyles';
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet, View, Text, Button, FlatList, TouchableOpacity
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import {
+    getDatabase, ref, onValue, query, orderByChild, equalTo, orderByKey, get
+} from 'firebase/database';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import app from '../../firebase';
+import { challengeTypes } from '../utils/challengeTypes';
+import { white, primaryColor, secondaryColorDarker } from '../utils/globalStyles';
 
-export default function ActiveChallenges() {
+export default function ActiveChallenges({ user }) {
+    const navigation = useNavigation();
+    const [state, setState] = useState({
+        id: user ? Object.keys(user)[0] : 'no user logged in',
+        list: [],
+        showList: false
+    });
+
+    const getList = () => {
+        const db = getDatabase(app);
+        const challengeUsersRef = ref(db, 'challengeUsers/');
+        // Hard coded user key
+        const userChallenges = query(challengeUsersRef, orderByChild('userIdentifier'), equalTo("-N0YbP7d2OEgbYQ6EIAo"));
+        const challengesList = []
+        onValue(userChallenges, (snapshot0) => {
+            snapshot0.forEach((childSnapshot0) => {
+                const challengesRef = ref(db, 'challenges/');
+                const challenges = query(challengesRef, orderByKey(), equalTo(childSnapshot0.val().challengeIdentifier));
+                onValue(challenges, (snapshot) => {
+                    snapshot.forEach((childSnapshot) => {
+                        const challengeGoalsRef = ref(db, 'challengeGoals/');
+                        const challengeGoals = query(challengeGoalsRef, orderByChild('challengeIdentifier'), equalTo(childSnapshot.key));
+                        const goalsList = [];
+                        onValue(challengeGoals, (snapshot2) => {
+                            snapshot2.forEach((childSnapshot2) => {
+                                const goalRef = ref(db, `goals/${childSnapshot2.val().goalsIdentifier}/description`);
+                                onValue(goalRef, (snapshot3) => {
+                                    goalsList.push(snapshot3.val());
+                                });
+                            });
+                        });
+                        const challengeTagsRef = ref(db, 'challengeTags/');
+                        const challengeTags = query(challengeTagsRef, orderByChild('challengeIdentifier'), equalTo(childSnapshot.key));
+                        const tagsList = [];
+                        onValue(challengeTags, (snapshot4) => {
+                            snapshot4.forEach((childSnapshot4) => {
+                                const tagRef = ref(db, `tags/${childSnapshot4.val().tagsIdentifier}/description`);
+                                onValue(tagRef, (snapshot5) => {
+                                    tagsList.push(`#${snapshot5.val()}`);
+                                });
+                            });
+                        });
+                        const challengeBadgesRef = ref(db, 'challengeBadges/');
+                        const challengeBadges = query(challengeBadgesRef, orderByChild('challengeIdentifier'), equalTo(childSnapshot.key));
+                        const badgesList = [];
+                        onValue(challengeBadges, (snapshot6) => {
+                            snapshot6.forEach((childSnapshot7) => {
+                                const badgeRef = ref(db, `badges/${childSnapshot7.val().badgesIdentifier}/description`);
+                                onValue(badgeRef, (snapshot8) => {
+                                    badgesList.push(snapshot8.val());
+                                });
+                            });
+                        });
+                        challengesList.push({
+                            id: childSnapshot.key,
+                            description: childSnapshot.val().description,
+                            date: childSnapshot.val().date,
+                            type: childSnapshot.val().type,
+                            goals: goalsList,
+                            tags: tagsList,
+                            badges: badgesList,
+                        });
+                    });
+                });
+            });
+        });
+        return challengesList;
+    };
+
+    useEffect(() => {
+        setState({
+            ...state,
+            list: getList()
+        });
+    }, []);
+
     return (
         <View style={styles.container}>
-            <Text>Active Challenges</Text>
+            <Text style={styles.header}>Active Challenges</Text>
+            {state.showList
+                ? (
+                    <FlatList
+                        data={state.list}
+                        keyExtractor={(key) => {
+                            return key.id;
+                        }}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity
+                                style={styles.listItemWrapper}
+                                key={item.id}
+                                onPress={() => navigation.navigate('Challenge Detail', { challenge: item, userId: state.id })}
+                            >
+                                <Text style={styles.description}>{item.description}</Text>
+                                <Text style={styles.item}>
+                                    <Text style={styles.bold}>Type:&nbsp;</Text>
+                                    {challengeTypes.find((x) => x.id === item.type).item}
+                                </Text>
+                                {/* <Text style={styles.item}> */}
+                                {/*    Created: */}
+                                {/*    {item.date} */}
+                                {/* </Text> */}
+                                <Text style={styles.item}>
+                                    <Text style={styles.bold}>Goals:&nbsp;</Text>
+                                    {item.goals.map((goal, index) => {
+                                        return (
+                                            <Text key={goal}>
+                                                {goal}
+                                                {index < item.goals.length - 1 ? ', ' : ''}
+                                            </Text>
+                                        );
+                                    })}
+                                </Text>
+                                <Text style={styles.item}>
+                                    <Text style={styles.bold}>Tags:&nbsp;</Text>
+                                    {item.tags.map((tag, index) => {
+                                        return (
+                                            <Text key={tag}>
+                                                {tag}
+                                                {index < item.tags.length - 1 ? ', ' : ''}
+                                            </Text>
+                                        );
+                                    })}
+                                </Text>
+                            </TouchableOpacity>
+                        )}
+                    />
+                ) : (
+                    <TouchableOpacity onPress={() => setState({ ...state, showList: true })}>
+                        <Button
+                            title='Show Active Challenges'
+                            color={primaryColor}
+                            accessibilityLabel='See Active Challenges button'
+                        />
+                        <Ionicons name='arrow-down-outline' color={primaryColor} size={30} style={styles.icon} />
+                    </TouchableOpacity>
+                )}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    header: {
+        fontSize: 30,
+        marginBottom: 20,
+        fontWeight: '500'
+    },
     container: {
         flex: 1,
         backgroundColor: white,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: '15%'
+    },
+    addNewButtonWrapper: {
+        backgroundColor: primaryColor,
+        borderRadius: 30,
+        padding: 10,
+        marginBottom: 20
+    },
+    listItemWrapper: {
+        padding: 20,
+        paddingLeft: 10,
+        margin: 10,
+        backgroundColor: secondaryColorDarker,
+        borderRadius: 10,
+        shadowRadius: 1,
+        shadowOpacity: 0.5,
+        shadowOffset: {
+            width: 1,
+            height: 1
+        },
+    },
+    description: {
+        fontSize: 20,
+        fontWeight: '500',
+        shadowOpacity: 0,
+        marginBottom: 10,
+        color: white
+    },
+    bold: {
+        fontWeight: '600'
+    },
+    item: {
+        color: white
+    },
+    icon: {
+        alignSelf: 'center'
     },
 });
